e380491c6d228a9418b347dbe01e99c22e76d1de
Maurice Schmidt <maurice.schmidt.mail@web.de>
2026-05-11 13:16:49 +0200

    spaced path

--- py_geo_code_parser.py
+++ py_geo_code_parser.py
@@ -10,8 +10,8 @@ Düse beim Rotieren denselben Punkt berührt.
 """
 
 # Ein-/Ausgabe
-INPUT_PATH = 'output_trajektorie/Kegel_v6_5x2.txt'
-OUTPUT_PATH = 'output_geo_code/Kegel_v6_5x2.geo'
+INPUT_PATH = 'output_trajektorie/Kegel_v6_5x1.txt'
+OUTPUT_PATH = 'output_geo_code/Kegel_v6_5x1_space.geo'
 
 # Geschwindigkeitskonstante (mm/min)
 SPEED_CONSTANT = 4.2 * 60  # 8.4 mm/s -> 504 mm/min
@@ -29,7 +29,7 @@ BED_OFFSET_Z = -186.25046989
 # Offset zur Extruder-Position für Testzwecke
 TEST_OFFSET_X = 0
 TEST_OFFSET_Y = 0
-TEST_OFFSET_Z = 0 # -20 für Testzwecke, damit die Düse nicht auf dem Bett schleift
+TEST_OFFSET_Z = -20 # -20 für Testzwecke, damit die Düse nicht auf dem Bett schleift
 
 # Extruder-Temperatur und temperaturabhängiger Z-Offset
 # Offset steigt linear von 0 mm bei 0 °C an: offset_z = TEMP_OFFSET_Z_SLOPE * EXTRUDER_TEMPERATURE

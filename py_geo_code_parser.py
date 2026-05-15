b6d69ab4f1159203df9fc43a6c896759f8abdbbc
Maurice Schmidt <maurice.schmidt.mail@web.de>
2026-05-15 15:47:06 +0200

    qik fix 2

--- py_geo_code_parser.py
+++ py_geo_code_parser.py
@@ -10,8 +10,8 @@ Düse beim Rotieren denselben Punkt berührt.
 """
 
 # Ein-/Ausgabe
-INPUT_PATH = 'output_trajektorie/Kegel_v6_5x1_fine.txt'
-OUTPUT_PATH = 'output_geo_code/Kegel_v6_5x1+3_fine.geo'
+INPUT_PATH = 'output_trajektorie/Kegel_v6_5x1_finer.txt'
+OUTPUT_PATH = 'output_geo_code/Kegel_v6_5x1+2_finer.geo'
 
 # Geschwindigkeitskonstante (mm/min)
 SPEED_CONSTANT = 8.7 * 60
@@ -29,7 +29,7 @@ BED_OFFSET_Z = -160.01789573
 # Offset zur Extruder-Position für Testzwecke
 TEST_OFFSET_X = 0
 TEST_OFFSET_Y = 0
-TEST_OFFSET_Z = 3.0  # 20 für Testzwecke, damit die Düse nicht auf dem Bett schleift
+TEST_OFFSET_Z = 2.0  # 20 für Testzwecke, damit die Düse nicht auf dem Bett schleift
 
 # Extruder-Temperatur und temperaturabhängiger Z-Offset
 # Offset steigt linear von 0 mm bei 0 °C an: offset_z = TEMP_OFFSET_Z_SLOPE * EXTRUDER_TEMPERATURE
